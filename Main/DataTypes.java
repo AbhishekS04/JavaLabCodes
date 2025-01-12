3a7b6bd682c240c55f84de686706348b3da6891b
AbhishekS04 <abhishek23main@gmail.com>
2025-01-13 00:24:32 +0530

    Refactor Java examples: remove Main.java and add DataTypes.java with comprehensive data type demonstrations

---
+++ Main/DataTypes.java
@@ -0,0 +1,47 @@
+// ! Java Data Types
+
+/*
+ * (byte) Stores whole numbers from -128 to 127
+ * (short) Stores whole numbers from -32,768 to 32,767
+ * (int) Stores whole numbers from -2,147,483,648 to 2,147,483,647
+ * (long) Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+ * (float) Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+ * (double) Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+ * (boolean) Stores true or false values
+ * (char) Stores a single character/letter or ASCII values
+ */
+
+public class DataTypes {
+
+    public static void main(String args[]) {
+
+        // byte myByte = 100;
+        // System.out.println("The byte is: " + myByte);
+        // short myShort = 5000;
+        // System.out.println("The Short is: " + myShort);
+        // int myInt = 100;
+        // System.out.println("The Int is: " + myInt);
+        // long myLong = 100;
+        // System.out.println("The Long is: " + myLong);
+        // float myFloat = 200.00f;
+        // System.out.println("The float is: " + myFloat);
+        // double myDouble = 100.99d;
+        // System.out.println("The double is: " + myDouble);
+        // char myChar = 'A';
+        // System.out.println("The char is: " + myChar);
+        // String myString = "Abhishek Singh";
+        // System.out.println("The string is: " + myString);
+
+
+        //! examples 
+
+        float f1=15.99f;
+        System.out.println("The Float Value Is: "+f1);
+
+        //! Why the value is 120000.0 because the E4 is a scientific notation and it is equal to 10^4
+
+        double d1=12E4d;
+        System.out.println("The Float Value Is: "+d1);
+
+    }
+}
