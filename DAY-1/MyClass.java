8b1b127f735931eb913680a671e6d75087a54712
AbhishekS04 <abhishek23main@gmail.com>
2025-01-15 00:39:57 +0530

    Refactor MyClass.java: update main method to display hardcoded values instead of command-line arguments

--- DAY-1/MyClass.java
+++ DAY-1/MyClass.java
@@ -1,31 +1,21 @@
-
 // class MyClass {
-//     public static void main(String args[] ) {
-//         System.out.println("Abhishek Singh");
-//         System.out.println("Department: Computer Science and Engineering");
-//         System.out.println("Roll Number: UG/02/BTCSE/2023/011");
-//         System.out.println("Sec: A");
+//     public static void main(String args[]) {
+
+//         System.out.println("Name:" + args[0]);
+//         System.out.println("Roll:" + args[1]);
+//         System.out.println("Sec:" + args[2]);
+
 //     }
 // }
 
 class MyClass {
-    public static void main(String args[]) {
-
-        System.out.println("Name:" + args[0]);
-        System.out.println("Roll:" + args[1]);
-        System.out.println("Sec:" + args[2]);
-
+    public static void main(String args[] ) {
+        System.out.println("Name: Abhishek Singh");
+        System.out.println("Department: Computer Science and Engineering");
+        System.out.println("Roll Number: UG/02/BTCSE/2023/011");
     }
 }
 
+
 // ! output
 
-// ? javac MyClass.java
-// ? java MyClass "Abhishek" "011" "A"
-/*
-
- * ? Name:Abhishek singh
- * ? Roll:011
- * ? Sec:A
-  
- */
