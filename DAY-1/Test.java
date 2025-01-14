241dff5212391fae42d67ebc7b2f0eb2f5b50b45
AbhishekS04 <abhishek23main@gmail.com>
2025-01-15 00:58:42 +0530

    Fix typos in comments and improve example command line arguments in Test.java

--- DAY-1/Test.java
+++ DAY-1/Test.java
@@ -1,10 +1,12 @@
 public class Test {
+
     public static void main(String[] args) {
         System.out.println(args.length > 0 ? args[0] : "Enter argument");
         System.out.println(args.length > 1 ? args[1] : "Enter argument");
         System.out.println(args.length > 2 ? args[2] : "Enter argument");
     }
 }
+
 // - List 3 favorite foods.  
 // - List 3 favorite books.  
 // - List 3 favorite places.
@@ -12,11 +14,11 @@ public class Test {
 //! java Test Favorite Foods:
 
 /*
-! cammand line argument text 
-java Test "Favorite Foods: Pizza,Burger,Pasta" "Favorite Books: Jani na,365 days,Atomic Habbits" "Favorite Places: Kolkata,Durgapur,Rampurhat"   
+! command line argument text 
+java Test "Favorite Foods: Pizza,Burger,Pasta" "Favorite Books:Ikigai,Philosophy of Money, Atomic Habits" "Favorite Places: Kolkata,Durgapur,Rampurhat"   
 
 ? Favorite Foods: Pizza,Burger,Pasta
-? Favorite Books: Jani na,365 days,Atomic Habbits
-? Favorite Places: Kolkata,Durgapur,Rampurhat
+? Favorite Books: Ikigai,Philosophy of Money, Atomic Habits
+? Favorite Places: Kolkata, Durgapur, Rampurhat
 
- */
+*/
