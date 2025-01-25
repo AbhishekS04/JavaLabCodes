2671b91704ac5e11d2d0148d829513b88f9fc988
AbhishekS04 <abhishek23main@gmail.com>
2025-01-25 15:28:02 +0530

    Add JavaLoops class with examples of while, do-while, and for loops

---
+++ MyPrac-Main/JavaLoops.java
@@ -0,0 +1,34 @@
+
+//! while loop
+
+public class JavaLoops {
+    public static void main(String[] args) {
+        // int i = 0;
+        // while (i < 4) {
+        // System.out.print((" ")+i);
+        // i++;
+        // }
+
+        // ! do while loop
+
+        // int i = 0;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while (i < 5);
+
+        //! for loop
+
+
+        // for (int j = 0; j <= 5; j++) {
+        //     System.out.print((" ")+j);
+        // }
+
+        //! another for loop example
+
+        for(int i=0; i<5; i=i+1){
+            System.out.println(i);
+
+        }
+    }
+}
