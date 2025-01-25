6219518a38705df3802abf64577e3b414c64bd65
AbhishekS04 <abhishek23main@gmail.com>
2025-01-25 15:44:51 +0530

    Refactor JavaLoops.java to enhance loop examples and improve code readability

--- MyPrac-Main/JavaLoops.java
+++ MyPrac-Main/JavaLoops.java
@@ -13,22 +13,52 @@ public class JavaLoops {
 
         // int i = 0;
         // do {
-        //     System.out.println(i);
-        //     i++;
+        // System.out.println(i);
+        // i++;
         // } while (i < 5);
 
-        //! for loop
-
+        // ! for loop
 
         // for (int j = 0; j <= 5; j++) {
-        //     System.out.print((" ")+j);
+        // System.out.print((" ")+j);
         // }
 
-        //! another for loop example
+        // ! another for loop example
 
-        for(int i=0; i<5; i=i+1){
-            System.out.println(i);
+        // for(int i=0; i<=6; i=i+3){
+        // System.out.print(" " + i);
 
-        }
+        // }
+
+        // ! nested loop
+
+        // for(int i=1; i<=2; i++){
+        // System.out.println("Outer loop:"+i);
+
+        // for(int j=1; j<=3; j++){
+        // System.out.println("Inner loop:"+j);
+        // }
+
+        // }
+
+        // ! For-Each Loop
+
+        // String [] cars ={ "Volvo", "BMW", "Ford", "Mazda"};
+        // for (String i : cars){
+        // System.out.print(" "+i);
+        // }
+
+        // ! real life example
+      
+        // int number = 11;
+
+        // for (int i = 1; i <= 10; i++) {
+        //     System.out.println(number + " x " + i + " = " + (number * i));
+        // }
+
+        //! java break statement
+
+        
     }
+
 }
