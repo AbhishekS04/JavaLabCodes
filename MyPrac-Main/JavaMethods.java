86ea0bbb8c32703d9fc96865c838356a3cc3eb6a
AbhishekS04 <abhishek23main@gmail.com>
2025-01-26 22:15:12 +0530

    Add checkAge method to JavaMethods.java for age eligibility demonstration

--- MyPrac-Main/JavaMethods.java
+++ MyPrac-Main/JavaMethods.java
@@ -21,29 +21,41 @@ public class JavaMethods {
     // ! Java Method Parameters
 
     // static void MyMethod(String fname) {
-    //     System.out.println(fname + " is the best car in the world");
+    // System.out.println(fname + " is the best car in the world");
     // }
 
     // public static void main(String[] args) {
-    //     MyMethod("Bugatti");
-    //     MyMethod("Lamborghini");
-    //     MyMethod("Ferrari");
+    // MyMethod("Bugatti");
+    // MyMethod("Lamborghini");
+    // MyMethod("Ferrari");
 
     // }
 
-   //! Multiple Parameters example
-    
-//    static void MyMethod(String fname,int age) {
-//         System.out.println("The age of "+fname + " is " +age);
-//     }
+    // ! Multiple Parameters example
 
-//     public static void main(String[] args) {
-//         MyMethod("Bristi", 19);
-//         MyMethod("Abhishek", 21);
-//         MyMethod("Snehasish", 20);
-//         MyMethod("Tushar", 20);
-//     }
+    // static void MyMethod(String fname,int age) {
+    // System.out.println("The age of "+fname + " is " +age);
+    // }
 
+    // public static void main(String[] args) {
+    // MyMethod("Bristi", 19);
+    // MyMethod("Abhishek", 21);
+    // MyMethod("Snehasish", 20);
+    // MyMethod("Tushar", 20);
+    // }
 
+    // ! A Method with If...Else
+
+    static void checkAge(int age) {
+        if (age < 18) {
+            System.out.println("You are not eligible for vote");
+        } else {
+            System.out.println("You are eligible for vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkAge(18);
+    }
 
 }
