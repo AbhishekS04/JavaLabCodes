9c8b7b7d89753942637b7b743ff3ebf830fc1065
AbhishekS04 <abhishek23main@gmail.com>
2025-01-26 22:02:19 +0530

    Refactor JavaMethods.java to comment out old method examples and add a new method demonstrating method parameters

--- MyPrac-Main/JavaMethods.java
+++ MyPrac-Main/JavaMethods.java
@@ -1,10 +1,34 @@
 public class JavaMethods {
-    static void myMethod() {
-        System.out.println("I am Abhishek singh!");
+    // static void myMethod() {
+    // System.out.println("I am Abhishek singh!");
+
+    // }
+    // public static void main(String[] args)
+    // {
+    // myMethod();
+    // }
+
+    // ! A method can also be called multiple times
+
+    // static void MyMethod(){
+    // System.out.println("Hi i will exicute three times");
+    // }
+    // public static void main(String[] args){
+    // MyMethod();
+    // MyMethod();
+    // MyMethod();
+    // }
+    // ! Java Method Parameters
+
+    static void MyMethod(String frame) {
+        System.out.println(frame + " is the best car in the world");
+    }
+
+    public static void main(String[] args) {
+        MyMethod("Bugatti");
+        MyMethod("Lamborghini");
+        MyMethod("Ferrari");
 
     }
-    public static void main(String[] args)
-    {
-        myMethod();
-    }
+
 }
