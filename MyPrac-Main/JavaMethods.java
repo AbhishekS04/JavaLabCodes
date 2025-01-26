e6aefab4f63a31e8baa865dbaef62f7f49086602
AbhishekS04 <abhishek23main@gmail.com>
2025-01-26 21:56:39 +0530

    Add myMethod to JavaMethods.java for demonstration purposes

--- MyPrac-Main/JavaMethods.java
+++ MyPrac-Main/JavaMethods.java
@@ -1,3 +1,10 @@
 public class JavaMethods {
-    
+    static void myMethod() {
+        System.out.println("I am Abhishek singh!");
+
+    }
+    public static void main(String[] args)
+    {
+        myMethod();
+    }
 }
