d742418195261b74e0d6fab0f2d21e108ae058cc
AbhishekS04 <abhishek23main@gmail.com>
2025-01-26 22:10:49 +0530

    Refactor JavaMethods.java to comment out old method examples and add multiple parameters example

--- MyPrac-Main/JavaMethods.java
+++ MyPrac-Main/JavaMethods.java
@@ -20,15 +20,30 @@ public class JavaMethods {
     // }
     // ! Java Method Parameters
 
-    static void MyMethod(String frame) {
-        System.out.println(frame + " is the best car in the world");
-    }
+    // static void MyMethod(String fname) {
+    //     System.out.println(fname + " is the best car in the world");
+    // }
 
-    public static void main(String[] args) {
-        MyMethod("Bugatti");
-        MyMethod("Lamborghini");
-        MyMethod("Ferrari");
+    // public static void main(String[] args) {
+    //     MyMethod("Bugatti");
+    //     MyMethod("Lamborghini");
+    //     MyMethod("Ferrari");
 
-    }
+    // }
+
+   //! Multiple Parameters example
+    
+//    static void MyMethod(String fname,int age) {
+//         System.out.println("The age of "+fname + " is " +age);
+//     }
+
+//     public static void main(String[] args) {
+//         MyMethod("Bristi", 19);
+//         MyMethod("Abhishek", 21);
+//         MyMethod("Snehasish", 20);
+//         MyMethod("Tushar", 20);
+//     }
+
+
 
 }
