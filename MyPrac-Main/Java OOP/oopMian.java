f0c5183e5074a64131a374d682009c2c9155603d
AbhishekS04 <abhishek23main@gmail.com>
2025-01-27 21:35:09 +0530

    Add oopMian class with main method to demonstrate object instantiation and variable access

---
+++ MyPrac-Main/Java OOP/oopMian.java
@@ -0,0 +1,9 @@
+
+
+public class oopMian {
+    int x = 5;
+    public static void main(String[] args) {
+        oopMian myobj =new oopMian();
+        System.out.println("The value of MYOBJ is: "+myobj.x);
+    }
+}
