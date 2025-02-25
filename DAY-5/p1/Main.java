2d0fca84a57ecf91c1a8ccf95f98491d3b7a2ca6
AbhishekS04 <abhishek23main@gmail.com>
2025-02-25 22:27:54 +0530

    Add Java programs for interface implementation and package creation

---
+++ DAY-5/p1/Main.java
@@ -0,0 +1,12 @@
+// File: /b:/JavaLabCodes/DAY-5/p1/Main.java
+
+public class Main {
+    public void printStatement() {
+        System.out.println("In package p1");
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.printStatement();
+    }
+}
